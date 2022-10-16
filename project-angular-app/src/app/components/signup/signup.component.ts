6f6869f8717b3ba004068e96e503afffa9b8647e
Rinrada Wattanachanasorn <rinrada_lady27@outlook.com>
2022-10-16 20:42:25 +0700

    add profilemodal

--- project-angular-app/src/app/components/signup/signup.component.ts
+++ project-angular-app/src/app/components/signup/signup.component.ts
@@ -13,14 +13,14 @@ export class SignupComponent implements OnInit {
   profileForm = new FormGroup({
     picture: new FormControl('',[Validators.required]),
     title: new FormControl('',[Validators.required]),
-    firstName:  new FormControl('',[Validators.required]),
-    lastName: new FormControl('',[Validators.required]),
-    sex: new FormControl('',[Validators.required]),
-    birthyear: new FormControl('',[Validators.required]),
+    firstName:  new FormControl('',[Validators.required,Validators.maxLength(30)]),
+    lastName: new FormControl('',[Validators.required,Validators.maxLength(30)]),
+    sex: new FormControl('',[Validators.required,Validators.maxLength(6)]),
+    birthyear: new FormControl('',[Validators.required,Validators.pattern('[0-9]{4}'),Validators.max(2021),Validators.min(1900)]),
     phonenumber: new FormControl('',[Validators.required,Validators.pattern('[0][0-9]{9}')]),
-    username: new FormControl('',[Validators.required]),
+    username: new FormControl('',[Validators.required,Validators.maxLength(12),Validators.minLength(5)]),
     email: new FormControl('',[Validators.required,Validators.email]),
-    password: new FormControl('',[Validators.required]),
+    password: new FormControl('',[Validators.required,Validators.minLength(4)]),
     confirmpass : new FormControl('',[Validators.required]),
   });
 
